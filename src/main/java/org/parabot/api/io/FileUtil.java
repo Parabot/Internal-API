32537fb92730e86e9cd5a274d5a3cec38a1ad245
JKetelaar <jeroen@ketelaar.me>
2017-02-03 20:49:23 +0100

    [FEATURE] Added FileUtil#delete

--- src/main/java/org/parabot/api/io/FileUtil.java
+++ src/main/java/org/parabot/api/io/FileUtil.java
@@ -88,4 +88,28 @@ public class FileUtil {
         }
         destination.close();
     }
+
+    public static void delete(File file) throws IOException {
+        if (file.isDirectory()) {
+            if (file.list().length == 0) {
+
+                file.delete();
+            } else {
+
+                String files[] = file.list();
+
+                for (String temp : files) {
+                    File fileDelete = new File(file, temp);
+                    delete(fileDelete);
+                }
+
+                if (file.list().length == 0) {
+                    file.delete();
+                }
+            }
+
+        } else {
+            file.delete();
+        }
+    }
 }
